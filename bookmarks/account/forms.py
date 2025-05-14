e3adae351c2b50b3e921dba9ebc72ccd252aa82a
Cho Seonghan <chosnhn1@gmail.com>
2025-05-14 17:57:45 +0900

    add register

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,6 +1,21 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    # validate two passwords in form
+    def clean_password2(self):
+        cleaned = self.cleaned_data
+        if cleaned['password'] != cleaned['password2']:
+            raise forms.ValidationError('Password don\'t match.')
+        return cleaned['password2']
--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -6,8 +6,19 @@ urlpatterns = [
     # path('login/', views.user_login, name='login' ),
 
     # Using django's view
-    path('login/', auth_views.LoginView.as_view(), name='login' ),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout' ),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password-change/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # path('pawword-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('pawword-reset/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_reset_done'),
+    # path('pawword-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('pawword-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    # Using contrib.auth.urls views for auth
+    path('', include('django.contrib.auth.urls')),
     
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
