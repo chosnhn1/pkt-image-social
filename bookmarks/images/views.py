28e8c067bbe86be7002f018da21fcb02b1921d8e
Cho Seonghan <chosnhn1@gmail.com>
2025-07-21 16:19:29 +0900

    add media url; add image list

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,9 +1,10 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_POST
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import ImageCreateForm
 from .models import Image
 
@@ -44,3 +45,24 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+
+    if images_only:
+        return render(request, 'images/image/list_images.html', {'section': 'images', 'images': images})
+    
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
-* Image social app
+# Image social app
+
+## Reference
+
+* Mele, J. 예제로 배우는 Django 4
+  * Chapter 4 through 7
+    4. 소셜 웹사이트 구축하기
+    5. 소셜 인증 구현하기
+    6. 웹사이트에서 콘텐츠 공유하기
+    7. 사용자 활동 추적하기
+
+## Structure
+
+* [bookmarks](/bookmarks/): Django Project
+  * [account](/bookmarks/account/): app for account, auth
+  * [bookmarks](/bookmarks/bookmarks/): project settings
+  * [images](/bookmarks/images/): app for image upload and social interactions (main business logic)  
+* [venv](/venv/): Python Virtual Environment
+  * Powershell `.\venv\Script\activate`
+  * Bash: load with `source venv/Script/activate`
+  * unload with `deactivate`
+* [readme](/readme.md): this markdown file
+* [requirements.txt](/requirements.txt): Python dependency
+  * load with `pip install -r requirements.txt`
+  * write with `pip freeze > requirements.txt`
