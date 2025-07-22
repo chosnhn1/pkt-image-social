725477c5841150840a539030eb13ce07608a6b10
Cho Seonghan <chosnhn1@gmail.com>
2025-07-22 17:12:44 +0900

    add relations btw users (follow); add user list and details - views and templates -

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+# profile model attached to user
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
@@ -10,3 +12,24 @@ class Profile(models.Model):
     def __str__(self):
         return f'Profile of {self.user.username}'
     
+# relaying model for user-to-user following
+class Contact(models.Model):
+    user_from = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # index & ordering by recent
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+    
+# Add field into user model dynamically
+# (it is not recommended though; Using custom user model is the best practice)
+user_model = get_user_model()
+user_model.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False))
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -163,3 +165,8 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 ]
+
+# Other way of set absolute url on model:
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
